d90d1569074937099ed4be5aafd514d82579e527
Matthew Graca <matthew.graca@gmail.com>
2025-03-24 18:49:19 -0700

    updated tests to survive singleton

--- app/src/test/java/com/example/weightliftingwarmup/SchemeManagerTest.kt
+++ app/src/test/java/com/example/weightliftingwarmup/SchemeManagerTest.kt
@@ -9,29 +9,15 @@ import org.junit.Assert.*
 
 class SchemeManagerTest {
     @Test
-    fun getWeightScheme_getsEmptyScheme(){
-        val sm = SchemeManager()
-        assertTrue(sm.getWeightScheme().isEmpty())
-    }
-
-    @Test
-    fun getPlateScheme_getsEmptyScheme(){
-        val sm = SchemeManager()
-        assertTrue(sm.getPlateScheme().isEmpty())
-    }
-
-    @Test
     fun createScheme_successfullyAddsValidWeightScheme(){
-        val sm = SchemeManager()
-        sm.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        SchemeManager.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
         val expected = listOf(45.0, 85.0, 125.0, 165.0, 205.0)
-        assertTrue(sm.getWeightScheme() == expected)
+        assertTrue(SchemeManager.getWeightScheme() == expected)
     }
 
     @Test
     fun createScheme_successfullyAddsValidPlateScheme(){
-        val sm = SchemeManager()
-        sm.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        SchemeManager.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
         val expected = listOf(
             listOf(),
             listOf(10.0, 10.0),
@@ -39,24 +25,22 @@ class SchemeManagerTest {
             listOf(55.0, 5.0),
             listOf(55.0, 25.0)
         )
-        val real = sm.getPlateScheme()
+        val real = SchemeManager.getPlateScheme()
         assertTrue("Expected: $expected, Actual: $real", real == expected)
     }
 
     @Test
     fun createScheme_successfullyReplacesWeightScheme(){
-        val sm = SchemeManager()
-        sm.createScheme(45.0, 305.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
-        sm.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        SchemeManager.createScheme(45.0, 305.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        SchemeManager.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
         val expected = listOf(45.0, 85.0, 125.0, 165.0, 205.0)
-        assertTrue(sm.getWeightScheme() == expected)
+        assertTrue(SchemeManager.getWeightScheme() == expected)
     }
 
     @Test
     fun createScheme_successfullyReplacesPlateScheme(){
-        val sm = SchemeManager()
-        sm.createScheme(45.0, 305.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
-        sm.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        SchemeManager.createScheme(45.0, 305.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        SchemeManager.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
         val expected = listOf(
             listOf(),
             listOf(10.0, 10.0),
@@ -64,14 +48,14 @@ class SchemeManagerTest {
             listOf(55.0, 5.0),
             listOf(55.0, 25.0)
         )
-        assertTrue(sm.getPlateScheme() == expected)
+        assertTrue(SchemeManager.getPlateScheme() == expected)
     }
 
     @Test
     fun createGreedyPlateScheme_createsValidPlateScheme(){
-        val sm = SchemeManager()
-        val real = sm.createGreedyPlateScheme(
-            listOf(45.0, 85.0, 125.0, 165.0, 205.0),
+        SchemeManager.createScheme(45.0, 205.0, 5, Setting.GREEDY, WeightSystem.POUNDS)
+        val real = SchemeManager.createGreedyPlateScheme(
+            SchemeManager.getWeightScheme(),
             WeightSystem.POUNDS
         )
         val expected = listOf(
@@ -86,8 +70,7 @@ class SchemeManagerTest {
 
     @Test
     fun greedyCoinChange_worksForValidSum(){
-        val sm = SchemeManager()
-        val real = sm.greedyCoinChange(80.0, WeightSystem.POUNDS)
+        val real = SchemeManager.greedyCoinChange(80.0, WeightSystem.POUNDS)
         val expected = listOf<Double>(55.0, 25.0)
         assertTrue("Expected: $expected, Actual: $real", real == expected)
     }
